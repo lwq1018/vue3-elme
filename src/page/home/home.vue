d0524fbf1ac137266a580103bd1b8adcf08cb38d
KevinLiu <6624744@qq.com>
2025-01-09 16:11:33 +0800

    将部分vue2 option风格代码改成setup风格代码

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -42,59 +42,57 @@
 
 <script>
 import headTop from '../../components/header/head'
-import {cityGuess, hotcity, groupcity} from '../../service/getData'
+import {cityGuess, hotcity as getHotcity, groupcity as getGroupcity} from '../../service/getData'
+import { computed, onMounted, ref } from 'vue';
 
 export default {
-    data(){
-        return{
-            guessCity: '',   //当前城市
-            guessCityid: '', //当前城市id
-            hotcity: [],     //热门城市列表
-            groupcity: {},   //所有城市列表
-        }
-    },
-
-	mounted(){
-	    // 获取当前城市
+    components:{
+	    headTop
+	},
+    setup(){
+        const guessCity = ref('') //当前城市
+        const guessCityid = ref('') //当前城市id
+        const hotcity = ref([]) //热门城市列表
+        const groupcity = ref({}) //所有城市列表
+        onMounted(() => {
+            // 获取当前城市
 	    cityGuess().then(res => {
-	        this.guessCity = res.name;
-	        this.guessCityid = res.id;
+	        guessCity.value = res.name;
+	        guessCityid.value = res.id;
 	    })
 	
 	    //获取热门城市
-	    hotcity().then(res => {
-	        this.hotcity = res;
+	    getHotcity().then(res => {
+	        hotcity.value = res;
 	    })
 	
 	    //获取所有城市
-	    groupcity().then(res => {
-	        this.groupcity = res;
+	    getGroupcity().then(res => {
+	        groupcity.value = res;
 	    })
-	},
+        });
+        const sortgroupcity = computed(() => {
+            let sortobj = {};
+            for (let i = 65; i <= 90; i++) {
+                if (groupcity.value[String.fromCharCode(i)]) {
+                    sortobj[String.fromCharCode(i)] = groupcity.value[String.fromCharCode(i)];
+                }
+            }
+            return sortobj
+        })
+        const reload = () => {
+            window.location.reload();
+        }
+        return {
+            guessCity,
+            guessCityid,
+            hotcity,
+            groupcity,
+            sortgroupcity,
+            reload
+        }
+    },
 	
-	components:{
-	    headTop
-	},
-	
-	computed:{
-	    //将获取的数据按照A-Z字母开头排序
-	    sortgroupcity(){
-	        let sortobj = {};
-	        for (let i = 65; i <= 90; i++) {
-	            if (this.groupcity[String.fromCharCode(i)]) {
-	                sortobj[String.fromCharCode(i)] = this.groupcity[String.fromCharCode(i)];
-	            }
-	        }
-	        return sortobj
-	    }
-	},
-	
-	methods:{
-	    //点击图标刷新页面
-	    reload(){
-	        window.location.reload();
-	    }
-	},
 }
 
 </script>
